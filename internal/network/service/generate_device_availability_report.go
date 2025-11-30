93a6aca9a74f2e5f8fbcc529aa3fe7a205a81c82
adhimaswaskita-nm <adhimas.waskita@digitaltelkom.id>
2025-11-30 14:55:42 +0700

    fix: excel format

--- internal/network/service/generate_device_availability_report.go
+++ internal/network/service/generate_device_availability_report.go
@@ -60,24 +60,71 @@ func (s *Service) GenerateDeviceAvailabilityReporting(from, to int) (string, err
 	xlsx := excelize.NewFile()
 	sheet1Name := "Sheet One"
 	xlsx.SetSheetName(xlsx.GetSheetName(1), sheet1Name)
+	// ip mgmt
+	xlsx.MergeCell(sheet1Name, "A1", "A3")
 	xlsx.SetCellValue(sheet1Name, "A1", "IP Address")
+
+	// month
+	xlsx.MergeCell(sheet1Name, "B1", "B3")
 	xlsx.SetCellValue(sheet1Name, "B1", "Month")
-	xlsx.SetCellValue(sheet1Name, "C1", "Percentage")
-	xlsx.SetCellValue(sheet1Name, "D1", "Uptime")
-	xlsx.SetCellValue(sheet1Name, "E1", "Downtime")
+
+	// availability table name
+	xlsx.MergeCell(sheet1Name, "C1", "J1")
+	// xlsx.MergeCell(sheet1Name, "C1", "F1")
+	xlsx.SetCellValue(sheet1Name, "C1", "Availability")
+
+	// ICMP PING table name
+	xlsx.MergeCell(sheet1Name, "C2", "F2")
+	xlsx.SetCellValue(sheet1Name, "C2", "ICMP / Ping")
+
+	// ICMP Availability utilization
+	xlsx.SetCellValue(sheet1Name, "C3", "Percentage (%)")
+	xlsx.SetCellValue(sheet1Name, "D3", "Uptime")
+	xlsx.SetCellValue(sheet1Name, "E3", "Downtime")
+	xlsx.SetCellValue(sheet1Name, "F3", "Undetected")
+
+	// SNMP Uptime table name
+	xlsx.MergeCell(sheet1Name, "G2", "J2")
+	xlsx.SetCellValue(sheet1Name, "G2", "SNMP Uptime")
+
+	// SNMP Uptime utilization
+	xlsx.SetCellValue(sheet1Name, "G3", "Percentage (%)")
+	xlsx.SetCellValue(sheet1Name, "H3", "Uptime")
+	xlsx.SetCellValue(sheet1Name, "I3", "Downtime")
+	xlsx.SetCellValue(sheet1Name, "J3", "Undetected")
 
 	row := 0
-	for _, v := range data {
-		if v.Data.IP == "" {
-			continue
+	lastPoint := ""
+	for i, v := range data {
+		startpoint := 4
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("A%d", row+startpoint), v.Data.IP)
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("B%d", row+startpoint), v.Data.Month)
+		// ICMP / PING
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("C%d", row+startpoint), v.Data.IcmpPing.Ratio)
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("D%d", row+startpoint), v.Data.IcmpPing.Uptime)
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("E%d", row+startpoint), v.Data.IcmpPing.Downtime)
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("F%d", row+startpoint), v.Data.IcmpPing.UnknownTime)
+
+		// SNMP Uptime
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("G%d", row+startpoint), v.Data.SnmpUptime.Ratio)
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("H%d", row+startpoint), v.Data.SnmpUptime.Uptime)
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("I%d", row+startpoint), v.Data.SnmpUptime.Downtime)
+		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("J%d", row+startpoint), v.Data.SnmpUptime.UnknownTime)
+		if i == len(data)-1 {
+			lastPoint = fmt.Sprintf("J%d", row+startpoint)
 		}
-		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("A%d", row+2), v.Data.IP)
-		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("B%d", row+2), v.Data.Month)
-		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("C%d", row+2), v.Data.IcmpPing.Ratio)
-		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("D%d", row+2), v.Data.IcmpPing.Uptime)
-		xlsx.SetCellValue(sheet1Name, fmt.Sprintf("E%d", row+2), v.Data.IcmpPing.Downtime)
 		row++
 	}
+
+	style, err := xlsx.NewStyle(`
+	{
+		"alignment":{"horizontal":"center","ident":1,"justify_last_line":true,"reading_order":0,"relative_indent":1,"shrink_to_fit":true,"vertical":"","wrap_text":false},
+		"border":[{"type":"left","color":"000000","style":1},{"type":"top","color":"000000","style":1},{"type":"bottom","color":"000000","style":1},{"type":"right","color":"000000","style":1}]
+	}`)
+	if err != nil {
+		logrus.Error("error set broder style : ", err)
+	}
+	xlsx.SetCellStyle(sheet1Name, "A1", lastPoint, style)
 
 	filename := fmt.Sprintf("%s-%s.xlsx", "device_availability", time.Now().Format(time.RFC822))
 	err = xlsx.SaveAs(fmt.Sprintf("./docs/%s", filename))
